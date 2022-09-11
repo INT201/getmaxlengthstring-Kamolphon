22c587ef4cd84c5d5c9e3fc5f5e718253f745ae7
Kamolphon <kamolphon.thon@kmutt.ac.th>
2022-09-11 17:24:03 +0700

    evekmp_

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,18 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  if (arrayOfString === null || arrayOfString === undefined) {
+    return undefined
+  }
+  let eve = [arrayOfString[0]]
+  for (let i=1 ; i<arrayOfString.length ; i++){
+    if (arrayOfString[i].length> eve[0].length) {
+      eve = [arrayOfString[i]]
+    }
+    else if(arrayOfString[i].length === eve[0].length) {
+      eve.push(arrayOfString[i]) 
+    }
+  }
+  return eve
 }
 module.exports = getMaxLengthString
